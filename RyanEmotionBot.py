033bce2bbeee7d2c3a5c52abdc19c924d2b69868
Ryan Stecher <spammehere86@gmail.com>
2012-10-07 08:34:06 -0400

    Cleaned up code.

--- RyanEmotionBot.py
+++ RyanEmotionBot.py
@@ -2,18 +2,17 @@
 ## Sentiment analysis tool
 ## Benjamin Gleitzman (gleitz@mit.edu)
 ##
+
 import sys
 import word_bag
 
 def create_dictionary():
+    # Part 1: Create the dictionary from the AFINN.txt file
     dictionary = {}
     f = open('AFINN.txt', 'r')
     for line in f.readlines():
         value_splitter = line.split("\t")
         dictionary[value_splitter[0]] = int(value_splitter[1].strip())
-        # Part 1: Create the dictionary from the AFINN.txt file
-        # A tab character in Python is '\t'
-        # YOUR CODE HERE
     j = open('MiscEmotions.txt', 'r')
     for line in j.readlines():
         value_splitter = line.split("\t")
@@ -30,31 +29,27 @@ USER_INPUT = ""
 MY_DICTIONARY = create_dictionary()
 
 def split_into_words(sentence):
-    input_list = sentence.split(" ")
     # Part 2: Split a sentence of words into individual words
-    # The split() function might be helpful
-    # YOUR CODE HERE
+    input_list = sentence.split(" ")
     return input_list
 
 def score(word):
+    # Part 3: Given a word, return its score
     if word in MY_DICTIONARY:
         word_score = MY_DICTIONARY[word]
     else:
         word_score= 0
-    # Part 3: Given a word, return its score
-    # Hint: use MY_DICTIONARY
-    # YOUR CODE HERE
     return word_score
 
 def score_words(words):
+    # Part 4: For each word, add it's score to the total score
     total_score = 0
     for word in words:
         total_score += score(word)
-        # Part 4: For each word, add it's score to the total score
-        # YOUR CODE HERE
     return total_score
 
 def analyze_sentiment(sentence):
+    # Part 5: Based on the total score, determine if the sentence is positive or negative
     words = split_into_words(sentence)
     score = score_words(words)
     print "Score is", score
@@ -73,15 +68,14 @@ def analyze_sentiment(sentence):
     elif score == -1:
         return "Today's bad but tomorrow might be better"
     elif score == -2:
-        return "If I were an animal I'd be a ferret. Cause no one likes ferrets"
+        return "If I were an animal I'd be a ferret. Cause no one likes ferrets."
     elif score == -3:
-        return "I wish I was emo, cause at least those guys have each other"
+        return "I wish I was emo, cause at least those guys have each other."
     elif score == -4:
-        return "I like playing tic-tac-toe on my wrists. With a knife"
+        return "I have nothing."
     elif score == -5:
-        return "F**k this sh*t, I'm going back to sleep"
+        return "I can't go on, I'm going back to sleep"
     elif score < -5:
-        return "On a good day, only most of your hopes and dreams are crushed into a fine powder"
-    # Part 5: Based on the total score, determine if the sentence is positive or negative
-    # YOUR CODE HERE
+        return "On a good day, only most of your hopes and dreams are crushed into a fine powder."
+    
 
